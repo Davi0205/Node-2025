f490ed58b81db13993a942612da93f58b873cf3b
Davi0205 <macedorodriguez.davi@gmail.com>
2025-07-15 19:33:36 -0500

    path y ls

--- 6.path.js
+++ 6.path.js
@@ -1,3 +1,17 @@
-const path = require('path');
+const path = require('node:path');
 
+// unir rutas con path.join
+console.log(path.sep)
 
+// unir rutas con pathc.join 
+const filePath = path.join('./contemt','subfolder','test.txt')
+console.log(filePath)
+
+const base = path.basename('/tmp/alfonso-secret/password.txt')
+console.log(base)
+
+const filemane = path.basename('/tmp/alfonso-secret/password.txt','.txt')
+console.log(filemane)
+
+const extension = path.extname('my.super.image.jpg')
+console.log(extension)
